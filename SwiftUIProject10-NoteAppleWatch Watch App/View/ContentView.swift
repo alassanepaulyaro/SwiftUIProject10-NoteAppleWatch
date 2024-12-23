026a9a0a449f76d6904de9f50ea32098d3e72858
Paul Yaro <alassane-paul.yaro@outlook.fr>
2024-12-23 10:12:07 +0100

    Implementing  CRUD for note using local storage

--- SwiftUIProject10-NoteAppleWatch Watch App/View/ContentView.swift
+++ SwiftUIProject10-NoteAppleWatch Watch App/View/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @AppStorage("lineCount") var lineCount: Int = 1
     @State private var notes: [Note] = [Note]()
     @State private var text: String = ""
     
@@ -34,18 +35,78 @@ struct ContentView: View {
                 }
                 
                 Spacer()
-                Text("\(notes.count)")
+                
+                if notes.count >= 1 {
+                    List {
+                        ForEach(0..<notes.count, id: \.self) { i in
+                            
+                            HStack {
+                                Capsule()
+                                    .frame(width: 4)
+                                    .foregroundColor(.accentColor)
+                                Text(notes[i].text)
+                                    .lineLimit(lineCount)
+                                    .padding(.leading, 5)
+                                
+                            }
+                        }
+                        .onDelete(perform: delete)
+                    }
+                } else {
+                    Spacer()
+                    Image(systemName: "note.text")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.gray)
+                        .opacity(0.25)
+                        .padding(25)
+                    Spacer()
+                }
                 
                 
             }
             .navigationTitle("Note")
+            .onAppear(perform: {
+                load()
+            })
         }
     }
     
     
     func save() {
-        dump(notes)
+        do {
+            let data = try JSONEncoder().encode(notes)
+            let url = getDocumentDirectory().appendingPathComponent("notes")
+            try data.write(to: url)
+        } catch {
+            print("Saving data has failed!")
+        }
     }
+    
+    func getDocumentDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+    
+    func load() {
+        DispatchQueue.main.async {
+            do {
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+                let data = try Data(contentsOf: url)
+                notes = try JSONDecoder().decode([Note].self, from: data)
+            } catch {
+                // Do nothing
+            }
+        }
+    }
+    
+    func delete(offsets: IndexSet) {
+        withAnimation {
+            notes.remove(atOffsets: offsets)
+            save()
+        }
+    }
+    
 }
 
 #Preview {
